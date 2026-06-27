34e69c7f84740eab2835d0dbb6414a1e0c3dc7c1
DanVano <98115971+DanVano@users.noreply.github.com>
2026-06-27 00:28:07 -0700

    feat(app): add MAUI shell

---
+++ Grocery_Sense/GrocerySense.App/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace GrocerySense.App;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new MainPage()) { Title = "GrocerySense.App" };
+	}
+}
---
+++ Grocery_Sense/GrocerySense.App/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace GrocerySense.App;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+		builder.Services.AddMauiBlazorWebView();
+		builder.Services.AddGrocerySenseServices();
+
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
